3283afd2075bcc6aab44850d3a22d630a40c8ca5
yeneily seijas <yeneilyseijas.itsu@gmail.com>
2026-09-10 12:37:20 -0400

    ererer

--- lib/game_config.dart
+++ lib/game_config.dart
@@ -2,5 +2,5 @@ abstract class GameConfig {
   static const int ancho = 40;
   static const int alto = 20;
   static const int tamanoPaleta = 4;
-  static const Duration frameRate = Duration(milliseconds: 40); // 20 FPS
+  static const Duration frameRate = Duration(milliseconds: 50); // 20 FPS
 }
